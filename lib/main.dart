c1cab3c7d7750a44cf2a7ca7e1cde41dcd61200b
ahmed56vipp <awde5041@gmail.com>
2026-06-14 04:24:42 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,14 +2,70 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart' show rootBundle;
 
-// هذا الكلاس لجلب البيانات من الملف الذي رفعته
-Future<List> loadQuranData() async {
-  final String response = await rootBundle.loadString('assets/quran_full.json');
-  final data = await json.decode(response);
-  return data['surahs']; // تأكد أن المفتاح هو "surahs" حسب محتوى ملفك
+void main() {
+  runApp(const MyApp());
 }
 
-// كود صفحة القراءة المحدث
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SurahListScreen(),
+    );
+  }
+}
+
+class SurahListScreen extends StatefulWidget {
+  const SurahListScreen({super.key});
+
+  @override
+  State<SurahListScreen> createState() => _SurahListScreenState();
+}
+
+class _SurahListScreenState extends State<SurahListScreen> {
+  List surahs = [];
+
+  @override
+  void initState() {
+    super.initState();
+    loadQuranData();
+  }
+
+  Future<void> loadQuranData() async {
+    final String response = await rootBundle.loadString('assets/quran_full.json');
+    final data = await json.decode(response);
+    setState(() {
+      surahs = data['surahs'];
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("القرآن الكريم")),
+      body: ListView.builder(
+        itemCount: surahs.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            title: Text(surahs[index]['name']),
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => SurahDetailsScreen(surah: surahs[index]),
+                ),
+              );
+            },
+          );
+        },
+      ),
+    );
+  }
+}
+
 class SurahDetailsScreen extends StatelessWidget {
   final Map surah;
   const SurahDetailsScreen({super.key, required this.surah});
