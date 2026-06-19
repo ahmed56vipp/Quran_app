5cccc88ebec942bdba4a282891d041b89dfe79e1
ahmed56vipp <awde5041@gmail.com>
2026-06-19 11:20:40 +0300

    Update utils.dart

--- lib/utils.dart
+++ lib/utils.dart
@@ -1,4 +1,4 @@
-hereString toArabicNumerals(int number) {
+String toArabicNumerals(int number) {
   const arabicDigits = ['٠', '١', '٢', '٣', '٤', '٥', '٦', '٧', '٨', '٩'];
   return number.toString().split('').map((char) {
     return arabicDigits[int.parse(char)];
